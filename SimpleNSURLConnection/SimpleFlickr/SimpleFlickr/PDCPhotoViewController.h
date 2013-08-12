6387014ce123de9c98083f683896adab1d404152
Kyle Oba <koba@mudphone.com>
2013-08-12 01:00:01 -1000

    Allow viewing image.

---
+++ SimpleNSURLConnection/SimpleFlickr/SimpleFlickr/PDCPhotoViewController.h
@@ -0,0 +1,15 @@
+//
+//  PDCPhotoViewController.h
+//  SimpleFlickr
+//
+//  Created by koba on 8/12/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCPhotoViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *photoURL;
+
+@end
---
+++ SimpleNSURLConnection/SimpleFlickr/SimpleFlickr/PDCPhotoViewController.m
@@ -0,0 +1,68 @@
+//
+//  PDCPhotoViewController.m
+//  SimpleFlickr
+//
+//  Created by koba on 8/12/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import "PDCPhotoViewController.h"
+
+@interface PDCPhotoViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView;
+
+@end
+
+@implementation PDCPhotoViewController
+
+- (void)setTitle:(NSString *)title
+{
+    super.title = title;
+}
+
+- (void)setPhotoURL:(NSURL *)photoURL
+{
+    _photoURL = photoURL;
+    [self resetImage];
+}
+
+- (void)resetImage
+{
+    if (self.scrollView) {
+        self.scrollView.contentSize = CGSizeZero;
+        self.imageView.image = nil;
+        [self.imageView removeFromSuperview];
+        
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.photoURL];
+        UIImage *image = [[UIImage alloc] initWithData:imageData];
+        if (image) {
+            self.scrollView.zoomScale = 1.0;
+            self.scrollView.contentSize = image.size;
+            self.imageView.image = image;
+            self.imageView.translatesAutoresizingMaskIntoConstraints = NO;
+            [self.scrollView addSubview:self.imageView];
+            
+            NSDictionary *viewsDictionary = @{@"imageView":self.imageView};
+            [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[imageView]|" options:0 metrics:nil views:viewsDictionary]];
+            [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[imageView]|" options:0 metrics:nil views:viewsDictionary]];
+        }
+    }
+}
+
+- (UIImageView *)imageView
+{
+    if (!_imageView) _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    _imageView.backgroundColor = [UIColor redColor];
+    return _imageView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.scrollView.delegate = self;
+    [self resetImage];
+}
+
+@end
