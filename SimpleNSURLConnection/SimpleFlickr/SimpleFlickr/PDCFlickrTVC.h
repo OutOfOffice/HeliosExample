ab960f583c303233c0f199b105a3123eeeaecac0
Kyle Oba <koba@mudphone.com>
2013-08-12 00:24:44 -1000

    Add base Flicker TVC.

---
+++ SimpleNSURLConnection/SimpleFlickr/SimpleFlickr/PDCFlickrTVC.h
@@ -0,0 +1,13 @@
+//
+//  PDCFlickrTVC.h
+//  SimpleFlickr
+//
+//  Created by koba on 8/12/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCFlickrTVC : UITableViewController
+
+@end
---
+++ SimpleNSURLConnection/SimpleFlickr/SimpleFlickr/PDCFlickrTVC.m
@@ -0,0 +1,54 @@
+//
+//  PDCFlickrTVC.m
+//  SimpleFlickr
+//
+//  Created by koba on 8/12/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import "PDCFlickrTVC.h"
+
+@interface PDCFlickrTVC ()
+
+@end
+
+@implementation PDCFlickrTVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+@end
