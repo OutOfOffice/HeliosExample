df6d9f5555f1351da58642669fd777883055a65c
Kyle Oba <koba@mudphone.com>
2013-08-13 04:46:27 -1000

    Add basic Notoriety app with Core Data model.

---
+++ NotesApp/Notoriety/Notoriety/PDCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PDCAppDelegate.h
+//  Notoriety
+//
+//  Created by koba on 8/13/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NotesApp/Notoriety/Notoriety/PDCViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDCViewController.h
+//  Notoriety
+//
+//  Created by koba on 8/13/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCViewController : UIViewController
+
+@end
